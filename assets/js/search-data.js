64802892aa6ee087e33e3c60cc07def38d6c84f1
Pantakit-Joseph <70746412+Pantakit-Joseph@users.noreply.github.com>
2025-03-17 23:04:26 +0000

    Deploying to gh-pages from @ Pantakit-Joseph/Pantakit-Joseph.github.io@2b46fc4cf30e14270fe000beab9cae5e85208c22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "I&#39;m Pantakit Malitong (Joseph), a software developer passionate about DevOps and exploring modern programming languages. Here&#39;s a summary of my education and technical skills.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
